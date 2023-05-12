a4af6293a993cd51c75521f13c06c59760e54854
shubham-ovo <shubham.agrawal@ovo.com>
2023-05-12 18:16:05 +0100

    deployment artifacts

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 1. Node Version - 16.20.0
 2. NPM 8.18.4
+
+
+### Ingress used
+
+[ingress-nginx](https://kubernetes.github.io/ingress-nginx/)
--- services/posts/src/index.ts
+++ services/posts/src/index.ts
@@ -3,8 +3,9 @@ import app from "./app";
 
 const start = async () => {
   const port = process.env.PORT || 5000;
+  const mongo_url:string | undefined = process.env.MONGO_DB_URL;
   try {
-    await mongoose.connect("mongodb://localhost:27017/posts-db");
+    await mongoose.connect(mongo_url ? mongo_url : "");
     console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
